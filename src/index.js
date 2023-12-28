0c3d10d8f3f9f2775025afa90e58e5b671f300ff
yogesh sharma <vishvadeep.0001@gmail.com>
2023-12-28 21:44:23 +0530

    cloudinary installed utility created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 // require ('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongodb connection failed !!", error);
+  });
 
 
 
@@ -29,27 +38,7 @@ connectDB();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
 /*
 import express from "express";
 const app = express();
